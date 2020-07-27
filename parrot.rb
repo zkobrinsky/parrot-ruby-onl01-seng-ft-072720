e7dea8a091025d25370dd887e098cf5f700ea8b5
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-27 17:45:38 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk")
-  puts phrase
-  return phrase
+  puts "#{phrase}"
+  return "#{phrase}"
 end
 
   
